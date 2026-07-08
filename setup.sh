d183f445979f088e0b1ee1f2110a4823f46dd15c
Takanori Nagahara <takanori.nagahara@gmail.com>
2026-07-08 17:57:30 +0900

    Add symlink for instructions.md in setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@ echo "start setup dot claude files"
 
 p=`pwd -P`
 echo "######start setup .claude######"
-
 ln -s $p/CLAUDE.md ~/.claude/CLAUDE.md
 ln -s $p/settings.json ~/.claude/settings.json
-
+ln -s $p/instructions.md ~/.claude/instructions.json
 echo "######finished setup .claude######"
