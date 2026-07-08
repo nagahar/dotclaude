fd16cd16fb5a86dfa4d3addd3fccb9eff54ba408
Takanori Nagahara <takanori.nagahara@gmail.com>
2026-07-08 17:55:06 +0900

    Add symlink for instructions.md in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 echo "######start setup .claude######"
 mklink "c:%HOMEPATH%\.claude\CLAUDE.md" "%~dp0\CLAUDE.md"
 mklink "c:%HOMEPATH%\.claude\setting.json" "%~dp0\setting.json"
+mklink "c:%HOMEPATH%\.claude\instructions.md" "%~dp0\instructions.md"
 echo "######finished setup .claude######"
 pause
